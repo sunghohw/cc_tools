3d4bd87e27b87d005804a2e1b9e8efb4b4316a0e
Sung Ho Hwang <sunghohw@andrew.cmu.edu>
2016-02-14 17:59:31 -0500

    2/14 (2)

--- cc_json_utils.py
+++ cc_json_utils.py
@@ -9,14 +9,15 @@ def make_cc_data_from_json(json_file):
     read = open(json_file, "r")
     json_data = json.load(read)
 
-    for level,leveldata in json_data.items():
+    for leveldata in json_data:
         cc_level = cc_data.CCLevel()
-        cc_level.level_number = leveldata["level"]
+        cc_level.level_number = int(leveldata["level"])
         cc_level.time = leveldata["time"]
         cc_level.num_chips = leveldata["num_chip"]
+        print(leveldata["upper_layer"])
         cc_level.upper_layer = leveldata["upper_layer"]
         cc_level.lower_layer = leveldata["lower_layer"]
-        for type_num,layer_data in leveldata["optional"]:
+        for type_num,layer_data in leveldata["optional"].items():
             if type_num == "type3": cc_level.add_field(cc_data.CCMapTitleField(layer_data))
             elif type_num == "type4": cc_level.add_field(cc_data.CCTrapControlsField(layer_data))
             elif type_num == "type5": cc_level.add_field(cc_data.CCCloningMachineControlsField(layer_data))
@@ -25,7 +26,7 @@ def make_cc_data_from_json(json_file):
             elif type_num == "type8": cc_level.add_field(cc_data.CCPasswordField(layer_data))
             elif type_num == "type10":
                 cc_monsters = []
-                for monster,monster_data in layer_data:
+                for monster,monster_data in layer_data.items():
                     cord = cc_data.CCCoordinate(monster_data[0],monster_data[1])
                     cc_monsters.append(cord)
                 cc_level.add_field(cc_data.CCMonsterMovementField(cc_monsters))
--- convert.py
+++ convert.py
@@ -8,7 +8,7 @@ from pprint import pprint
 
 with open('data/sunghohw_cc1.json') as data_file:
     data = json.load(data_file)
-pprint(data)
 
-#jsonData = cc_json_utils.make_cc_data_from_json("data/sunghohw_cc1.json")
-#cc_dat_utils.write_cc_data_to_dat(jsonData,"data/sunghohw_cc1.dat")
+cc_data = cc_json_utils.make_cc_data_from_json("data/sunghohw_cc1.json")
+print(str(cc_data))
+cc_dat_utils.write_cc_data_to_dat(cc_data,"data/sunghohw_cc1.dat")
